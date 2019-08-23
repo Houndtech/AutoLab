736154278a5594d418d72cb4983bb923a83fa1f2
Jason Helmick <jasonhelmick@icloud.com>
2019-08-23 13:20:33 -0700

    Readme - update for init-rel

---
+++ Lc.AutoLab/Lc.Autolab.psm1
@@ -0,0 +1,12 @@
+<# Notes:
+
+Author: Jason Helmick
+License: MIT
+  
+Disclaimer
+This sample code is provided without copyright and AS IS.  It is free for you to use and modify.
+#> 
+
+#Requires -version 7.0
+#Requires -runasadministrator
+
